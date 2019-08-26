65c2df91818a9ed75644b4ae7bfbbf0b8ed31365
Jonathan McNeal <jonathan.mcneal@my.tccd.edu>
2019-08-26 18:57:00 -0500

    Talked about syntax

---
+++ classwork/Section1/HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Definetely assigned
+            string name = "";
+
+            string name2 = Console.ReadLine ();
+
+            //name2 = Console.ReadLine ();
+
+            name2 = name = "Sue";
+
+            Console.WriteLine (name2);
+
+            Console.WriteLine("Hello World!");
+
+            int hours = 8;
+            double payRate = 15.25;
+
+            double totalPay = payRate * hours;
+
+            string fullName = "Fred" + " " + "Jones";
+
+           /* passing = (grade > 60)
+                ? true : false;*/
+        }
+    }
+}
