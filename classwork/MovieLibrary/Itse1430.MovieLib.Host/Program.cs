64a6eda3a789e944a4a43bd39c6d2e4c34bfd7d2
Jonathan McNeal <jonathan.mcneal@my.tccd.edu>
2019-09-11 18:24:30 -0500

    class 9/9

---
+++ classwork/MovieLibrary/Itse1430.MovieLib.Host/Program.cs
@@ -0,0 +1,23 @@
+﻿//global scope
+using System;
+using System.Windows.Forms;
+
+//namespace scope
+namespace Itse1430.MovieLib.Host
+{
+    //type scope
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        //local scope
+        static void Main ()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles ();
+            Application.SetCompatibleTextRenderingDefault (false);
+            Application.Run (new MainForm ());
+        }
+    }
+}
---
+++ labs/Lab1/Itse1430.Maze/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Jonathan McNeal
+//ITSE-1430-Fall2019
+//LAB1
+namespace Itse1430.Maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
